0f97a59ad9523d8e004792940cdf5164df4fc80e
2017Yang <1090582686@qq.com>
2017-10-18 10:15:24 -0700

    2017.1.15

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: shixiaohui
+    > Mail: shxh@hqyj.com 
+    > Created Time: Wed 18 Oct 2017 09:52:51 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("hello github\n");
+	return 0;
+}
